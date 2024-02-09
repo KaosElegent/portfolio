93cedfd6ff1ffeb3f80c1f03d6b9813410a23e45
Shrey Ajaykumar Bhatt <sabhatt2@myseneca.ca>
2024-02-09 14:03:57 -0500

    fixed src not being static in express

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const HTTP_PORT = process.env.PORT || 8080;
 
 app.listen(HTTP_PORT, () => console.log(`server listening on: ${HTTP_PORT}`));
 
-app.use(express.static('public'));
+app.use('/public', express.static(path.join(__dirname, "public")));
+app.use('/src', express.static(path.join(__dirname, "src")));
 
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
