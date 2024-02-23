3394eadd85e5035f393e401e954a0f932ccdb65f
Shrey Ajaykumar Bhatt <sabhatt2@myseneca.ca>
2024-02-22 21:55:37 -0500

    bug: tailwind config wasn't accurate with html location

--- server.js
+++ server.js
@@ -5,10 +5,11 @@ const HTTP_PORT = process.env.PORT || 8080;
 
 app.listen(HTTP_PORT, () => console.log(`server listening on: ${HTTP_PORT}`));
 
-app.use('/public', express.static(path.join(__dirname, "public")));
-app.use('/src', express.static(path.join(__dirname, "src")));
+app.use(express.static('public'));
+//app.use('/public', express.static(path.join(__dirname, "public")));
+//app.use('/src', express.static(path.join(__dirname, "src")));
 
 app.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'));
+    res.sendFile(path.join(__dirname, '/views/index.html'));
 });
 
