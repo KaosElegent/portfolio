40567366edbaa000175d70fa2d9839ed37dc8051
Shrey Ajaykumar Bhatt <sabhatt2@myseneca.ca>
2024-02-24 16:59:21 -0500

    feat: finished work on the about and index page; starting projects page

--- server.js
+++ server.js
@@ -6,13 +6,19 @@ const HTTP_PORT = process.env.PORT || 8080;
 app.listen(HTTP_PORT, () => console.log(`server listening on: ${HTTP_PORT}`));
 
 app.use(express.static('public'));
+app.use(express.static('src'));
 //app.use('/public', express.static(path.join(__dirname, "public")));
 //app.use('/src', express.static(path.join(__dirname, "src")));
 
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, '/views/index.html'));
 });
+
 app.get('/about', (req,res) => {
     res.sendFile(path.join(__dirname, '/views/about.html'));
+});
+
+app.get('/projects', (req,res) => {
+    res.sendFile(path.join(__dirname, '/views/projects.html'));
 });
 
