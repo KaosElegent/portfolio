c1ef6f56ce8a144ac6b0cbb951f3c02003a81949
Shrey Ajaykumar Bhatt <sabhatt2@myseneca.ca>
2024-02-24 11:00:21 -0500

    feat: about page updated

--- server.js
+++ server.js
@@ -12,4 +12,7 @@ app.use(express.static('public'));
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, '/views/index.html'));
 });
+app.get('/about', (req,res) => {
+    res.sendFile(path.join(__dirname, '/views/about.html'));
+});
 
