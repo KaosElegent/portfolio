7fa7d9a82489e1a14d290437a4e10ed021a2219c
Shrey Bhatt <shreybhatt130@yahoo.com>
2024-02-26 12:21:30 -0500

    fix: fixed ui differences from ubuntu to windows; removed; needs testing with dropdown menu on chrome-mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [`./views/**/*.html`], // all .html files
   theme: {
     extend: {
@@ -8,7 +9,9 @@ module.exports = {
         'mc-2': '#4F4557',
         'mc-3': '#6D5D6E',
         'mc-4': '#F4EEE0',
-        'mc-1-d' : '#282535',
+        'mc-1-d' : '#1F232D',
+        'mc-text' : '#AFB3C3',
+        'mc-text-d' : '#6B7080',
       },
       screens: {
         'bigger-phone': '580px',
