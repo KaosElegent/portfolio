493a60ebf3e70289e21f122dc07e6d0c11bdd21b
Shrey Ajaykumar Bhatt <sabhatt2@myseneca.ca>
2024-02-26 00:59:20 -0500

    feat/refactor: bunch of code refactored and perfected

--- public/js/navscroll.js
+++ public/js/navscroll.js
@@ -1,13 +1,12 @@
-const scrollline = document.querySelector('.scroll-line');
+const scrollline = document.querySelector(".scroll-line");
 
-      function fillscrollline(){
-        const windowHeight = window.innerHeight;
-        const fullHeight = document.body.clientHeight;
-        const scrolled = window.scrollY;
-        const percentScrolled = (scrolled / (fullHeight - windowHeight)) * 100;
+function fillscrollline() {
+  const windowHeight = window.innerHeight;
+  const fullHeight = document.body.clientHeight;
+  const scrolled = window.scrollY;
+  const percentScrolled = (scrolled / (fullHeight - windowHeight)) * 100;
 
-        scrollline.style.width = percentScrolled + '%';
-      };
+  scrollline.style.width = percentScrolled + "%";
+}
 
-      window.addEventListener('scroll', fillscrollline);
-
+window.addEventListener("scroll", fillscrollline);
